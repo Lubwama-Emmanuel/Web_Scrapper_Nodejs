45e5af561b23a09f4e320ba156231ad36cc6ae16
Rex Munil <lubwamaemmanuel1@gmail.com>
2022-09-10 12:01:57 +0300

    Final commit

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
 const app = express();
+
+const url = "https://www.theguardian.com/international";
+axios(url).then((response) => {
+  const html = response.data;
+  const $ = cheerio.load(html);
+  const articles = [];
+
+  $(".fc-item__title", html).each(function () {
+    const title = $(this).text();
+    const link = $(this).find('a').attr('href');
+    articles.push({title, link});
+    
+  });
+  console.log(articles);
+}).catch(err => {
+    console.log("An Error occured when trying to scrap the web:", err)
+});
 
 module.exports = app;
